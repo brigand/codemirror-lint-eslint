f274e612158acbb086b70f7c892192c7207223d8
Frankie Bagnardi <f.bagnardi@gmail.com>
2017-01-02 22:54:32 -0700

    adds eslint require

--- eslint-lint.js
+++ eslint-lint.js
@@ -2,17 +2,9 @@
 // Distributed under an MIT license: http://codemirror.net/LICENSE
 
 // Depends on eslint.js from https://github.com/eslint/eslint
-
-(function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"));
-  else if (typeof define == "function" && define.amd) // AMD
-    define(["../../lib/codemirror"], mod);
-  else // Plain browser env
-    mod(CodeMirror);
-})(function(CodeMirror) {
 "use strict";
-
+var CodeMirror = require('codemirror');
+var eslint = require('eslint');
   var defaultConfig = {
     "ecmaFeatures": {},
     "env": {
@@ -203,5 +195,3 @@
 		return "error";
 	}    
   }
-  
-});
